0baf76c818f37c68b8baad097d337e5579fe4f45
Kai Gonzalez <gkai70264@gmail.com>
2021-07-05 20:47:23 -0400

    Yessir first commit babi

---
+++ KaiCSConsole/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+namespace KaiCSConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Loading Classes...");
+            Thread.Sleep(1000);
+            // THREAD LOADING SEQUENCE
+            //
+            //
+            // get dllexecutor
+            Classes.OSDLLExecutor executor = new Classes.OSDLLExecutor();
+            Console.WriteLine("Loaded OSDLLExecutor");
+            Thread.Sleep(1000);
+            //build a console basic
+            Classes.CConsoleBuilder consoleBuilder = new Classes.CConsoleBuilder("Main", new string[] { "-v" });
+            Thread.Sleep(1000);
+            // add a print manager
+            Classes.PrintManagerShortener shortener = new Classes.PrintManagerShortener();
+            Thread.Sleep(2000);
+            // continue in future version in git
+        }
+    }
+}
